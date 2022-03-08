b6499d63c9a16d64abdf1043bbbc571b59d21d44
Heather Graham <heather.graham@yahoo.com>
2022-03-08 12:13:47 -0700

    updated title on webpage

--- index.js
+++ index.js
@@ -155,10 +155,9 @@ function updateHtml() {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <meta name="Description" content="Enter your description here" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="./style.css">
-        <title>Bootstrap Components</title>
+        <title>Team Profile Generator</title>
     </head>
     <body>
     <div class="jumbotron text-center jumbotron-fluid">
